68d3abc001d909880c5846099815f48ff13905fd
dfabbar2606 <danielotfb05@gmail.com>
2022-12-01 09:13:21 +0100

    seguir en casa

---
+++ day1/helloword.py
@@ -0,0 +1,25 @@
+"parte 1"
+print(3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+print(3**4)
+print(3%4)
+print(3//4)
+
+"parte 2"
+print('Daniel')
+print('fabregat Barragán')
+print('España')
+print('I am enjoying 30 days of python')
+
+"parte 3"
+type(10)
+type(9.8)
+type(3.14)
+type(4-4j)
+type(['Asabeneh', 'Python', 'Finland'])
+type('Daniel')
+type('Fabregat Barragán')
+type('España')
+
