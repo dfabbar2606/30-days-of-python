1545191020d3ba58fd8a266f27395894f7134912
dfabbar2606 <danielotfb05@gmail.com>
2023-03-16 08:31:59 +0100

    guardar

---
+++ day 1/helloword.py
@@ -0,0 +1,36 @@
+"parte 1"
+print(3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+print(3**4)
+print(3%4)
+print(3//4)
+
+"parte 2"
+print('Daniel')
+print('fabregat Barragán')
+print('España')
+print('I am enjoying 30 days of python')
+
+"parte 3"
+type(10)
+type(9.8)
+type(3.14)
+type(4-4j)
+type(['Asabeneh', 'Python', 'Finland'])
+type('Daniel')
+type('Fabregat Barragán')
+type('España')
+
+"parte 4"
+print['number integer'(-3,-2,-1,0,1,2,3)]
+print['number float'(-3.5,-2.67,1.1,3.5)]
+print['number complex'(1+1j,2+4j,3+6j)]
+
+print['string'('I hope you are enjoying the first day of 30DaysOfPython Challenge')]
+print['booleans'(True)]#si la comida esta en el microondas,esta caliente 
+print['booleans'(False)]#si el suelo esta mojado, pero esta soleado, es falso que esta lloviendo
+print['list'['naranja,manzana,pera,platano']]
+print['tuple'['daniel','pablo','mario','anabel']]#nombres
+print['set'(2,5,86,235,77,)]
